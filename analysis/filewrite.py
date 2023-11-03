26a3dea09b8bfb481663fab1627ff9541931e545
Walterairs <valtteri.ayras@edu.turkuamk.fi>
2023-11-03 10:00:10 +0200

    uncertainty analysis init

--- analysis/filewrite.py
+++ analysis/filewrite.py
@@ -8,9 +8,9 @@ def read_serial_and_write_to_file(port, baud_rate, file_name, num_points):
                 file.write(data + '\n')  # Write data to the file
 
 if __name__ == "__main__":
-    port_name = 'COM5'  # Replace with your COM port
+    port_name = 'COM3'  # Replace with your COM port
     baud_rate = 9600
-    file_to_write = 'serial_data.txt'
+    file_to_write = 'bright_n_dark.txt'
     number_of_data_points = 1000
 
     read_serial_and_write_to_file(port_name, baud_rate, file_to_write, number_of_data_points)
--- niq.ino
+++ niq.ino
@@ -9,18 +9,15 @@
  
 #define SENSOR_PIN 1
 int sensorValue = 0;
-PrintWriter output;
 // the setup function runs once when you press reset or power the board
 
 void setup() {
   Serial.begin(9600);
-  output = createWriter("data.txt");
 }
  
 // the loop function runs over and over again forever
 void loop() {
   sensorValue = analogRead(SENSOR_PIN);
   Serial.println(sensorValue, DEC);
-  output.println(sensorValue);
   delay(3); // 3 milliseconds
 }
